e09d4e19204ee1a8aa0c00558eeefe6914786821
AnGgIt86 <jarbull86@gmail.com>
2021-07-10 03:17:51 +0700

    NFS-Projects: Bump to 20210709 build
    LLVM commit: https://github.com/llvm/llvm-project/commit/b11d31eb
    Clang Version: 13.0.0
    Binutils version: 2.36.1
    Builder commit: https://github.com/AnGgIt86/NFS-TC/commit/a6097daee3294403e070a2bed9f8db041ed5317d
    
    Signed-off-by: AnGgIt86 <jarbull86@gmail.com>

--- lib/cmake/llvm/LLVMConfig.cmake
+++ lib/cmake/llvm/LLVMConfig.cmake
@@ -150,7 +150,7 @@ if(LLVM_ENABLE_ZLIB)
   find_package(ZLIB)
 endif()
 
-set(LLVM_ENABLE_LIBXML2 )
+set(LLVM_ENABLE_LIBXML2 1)
 if(LLVM_ENABLE_LIBXML2)
   find_package(LibXml2)
 endif()
@@ -199,7 +199,7 @@ set(LLVM_HAVE_OPT_VIEWER_MODULES 0)
 set(LLVM_CONFIGURATION_TYPES )
 set(LLVM_ENABLE_SHARED_LIBS OFF)
 
-set(LLVM_DEFAULT_EXTERNAL_LIT "/drone/src/llvmTC/build/llvm/stage2/./bin/llvm-lit")
+set(LLVM_DEFAULT_EXTERNAL_LIT "/root/llvmTC/build/llvm/stage2/./bin/llvm-lit")
 set(LLVM_LIT_ARGS "-sv")
 
 set(LLVM_HAVE_LIBXAR "")
